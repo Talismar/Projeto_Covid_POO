afac4f9c510bdb1632d28e531f0f094927430f2f
andresanto5 <39716728+andresanto5@users.noreply.github.com>
2022-03-10 09:53:11 -0300

    Update main.py

--- POO/main.py
+++ POO/main.py
@@ -4,7 +4,7 @@ from DataProcessing import DataProcessing
 from Data import Data
 from dataAnalysis import dataAnalysis
 from dataVisualization import *
-from Prediction import *
+from prediction import *
 
 if __name__=='__main__':
 	"Opcional passar arquivo ou não - senão passar o algoritmo baixa"
